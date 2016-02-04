40473c91f32bda7d8b693bef7b077f69e5bdbf00
Andreas <andreas.mittelberger@univie.ac.at>
2016-02-04 10:14:17 +0100

    testing phph call

---
+++ html/php_test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 09:40:23 2016
+
+@author: mittelberger
+"""
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('user')
+
+args = parser.parse_args()
+
+print(args.user)
