6df81b6f181920055434e068a065a2f6bb9fb712
Andreas <andreas.mittelberger@univie.ac.at>
2016-02-16 19:27:12 +0100

    Made personal betting tables a symbolic link to actual file

--- html/users/andreas/index.php
+++ html/users/andreas/index.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Legendary Invention</title>
-  <link rel="stylesheet" href="../../legend.css" type="text/css">
- </head>
-<body>
-  Logged in as <?php $_SERVER['PHP_AUTH_USER'];?>
-
-  <h1>PLACE YOUR BETS!</h1>
-  
- <?php system(dirname(__FILE__).'/../../cgi/cgi_response.py '.'-f create_betting_table'.'-u '.$_SERVER['PHP_AUTH_USER']); ?>
-
-
- 
-</body>
-</html>
+../personal_betting_table.php
