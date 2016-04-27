08661fb2f55d9ef4b536e515214f4420f60b6009
giacomo <giak88@yahoo.it>
2016-04-27 14:03:23 +0200

    created betting rules

--- html/rules.php
+++ html/rules.php
@@ -16,6 +16,31 @@
 </div>
 
 <h3>betting rules</h3>
+<br>
 
+<span id="rules">
+ Welcome to the pnm-betting system! Here are the rules for betting:
+  <br>
+  <ul>
+   <li>Bets submission is possible at any time up to the starting time of each match (once the match has started the input field will be automatically changed to non editable)</li>
+   <li>You can modify previously submitted bets at any time (again before the match starts)</li>
+  </ul>
+  <br>
+  <br>
+ Points will be assigned as follows:
+  <br>
+  <ul>
+   <li>correct result will assign 3 points (e.g. result: 3-2, bet: 3-2 &#8594; 3 points)</li>
+   <li>correct outcome and correct goal difference will assign 2 points (e.g. result: 3-2, bet: 2-1 &#8594; 2 points)</li>
+   <li>correct outcome will assign 1 point (e.g. result: 3-2, bet: 2-0 &#8594; 1 point)</li>
+   <li>wrong outcome or no bet will assign 0 points (e.g. result: 3-2, bet: 0-1 &#8594; 0 point)</li>
+  </ul>
+  <br>
+ Obviously enough, at the end of the championship the user with most points wins.
+</span>
+
+<pre><h5>
+
+Time for you to start betting! Good luck!!!</h5></pre>
 </body>
 </html>
